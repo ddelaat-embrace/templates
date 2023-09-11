cb822f49336c9d46cd52a6989333caf902165d95
David DeLaat <ddelaat@embracepetinsurance.com>
2023-09-11 10:14:36 -0400

    Update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var baseCSSUrl = "https://ddelaat-embrace.github.io/templates/css/brands/";
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var brand = urlParams.get("brand") || "embrace";
+    var cssURL = baseCSSUrl + brand + ".css";
+
+    var linkElement = document.createElement("link");
+    linkElement.rel = "stylesheet";
+    linkElement.href = cssURL;
+    document.head.appendChild(linkElement);
+});
