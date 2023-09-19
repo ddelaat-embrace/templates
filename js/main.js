2f9db38d87a32cea522ba01db675fc20e11befef
David DeLaat <ddelaat@embracepetinsurance.com>
2023-09-19 13:41:00 -0400

    Add incoming brand to all links

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ var baseCSSUrl = "https://ddelaat-embrace.github.io/templates/css/brands/";
 
 var urlParams = new URLSearchParams(window.location.search);
 var brand = urlParams.get("brand") || "embrace";
-var cssURL = baseCSSUrl + brand + ".css";
 
+//--load the associated brand CSS
+var cssURL = baseCSSUrl + brand + ".css";
 var linkElement = document.createElement("link");
 linkElement.rel = "stylesheet";
 linkElement.href = cssURL;
 document.head.appendChild(linkElement);
+
+//--append the same brand param to any existing links
+document.querySelectorAll('a').forEach(linkElement => {
+    const linkUrl = new URL(linkElement.href, window.location.href);
+    linkUrl.searchParams.set('brand', brand);
+    linkElement.href = linkUrl.toString();
+});
